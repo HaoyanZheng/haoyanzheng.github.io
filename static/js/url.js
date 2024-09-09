2344041a86b89579c000aee913fd29b815401e98
Haoyan Zheng <zhenghaoyan0713@gmail.com>
2024-09-09 00:21:34 -0400

    Update url.js

--- static/js/url.js
+++ static/js/url.js
@@ -1,15 +1,3 @@
-function qqqun(){
-  window.open("https://jq.qq.com/?_wv=1027&k=3d7wk5gW");
-}
-
-function qqemail(){
+function email(){
   window.open("mailto:zhenghaoyan0713@qq.com")
-}
-
-function iwara(){
-    window.open("https://iwara-app.sanqianssr.com/")
-}
-
-function picacg(){
-    window.open("https://download.wikawika.xyz/apps/2.2.1.3.3.4_collections.apk")
 }
